169c795e799460ce1957ef103b7fbb1ac1ef7070
MMArephinAbid <71713365+MMArephinAbid@users.noreply.github.com>
2023-07-17 01:03:31 +0600

    Add files via upload

---
+++ tamim.c
@@ -0,0 +1,68 @@
+// A summary of the programms of the book Computer programming by Tamim shariar Subeen 
+
+// Introduction code
+
+// #include <stdio.h>
+// int main(){
+//     printf("hello world");
+//     return 0;
+// }
+
+// Chapter 2 - Data type, input & output 
+
+
+
+// FORMAT 1
+// #include <stdio.h>
+// int main(){
+//     int a;
+//     int b;
+//     int sum;
+//     a = 10;
+//     b = 20;
+//     sum = a*b;
+//     printf("Sum is %d",sum);
+//     return 0;
+// }
+
+
+// FORMAT 2
+// #include <stdio.h>
+// int main(){
+//     int a,b,sum;
+//     a = 50;
+//     b = 60;
+//     sum = a+b;
+//     printf("Sum is %d",sum);
+//     return 0;
+// }
+
+
+// FORMAT 3
+// #include <stdio.h>
+// int main(){
+//     int a = 50, b = 60,sum;
+//     sum = a+b;
+//     printf("sum is %d",sum);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//     int x,y;
+//     x =1;
+//     y =x;
+//     x =2;
+//     printf("%d",y);
+//     return 0;
+// }
+
+#include <stdio.h>
+int main(){
+    int a =50, b =60, sum;
+    sum = a+b;
+    printf("%d+%d = %d",a,b,sum);
+    return 0;
+}
+
